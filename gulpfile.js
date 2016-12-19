99944d1afe8ba3fb99110a6ce116a9ce5853f27a
Eric Pan <ericpanbot@gmail.com>
2016-12-19 00:01:03 -0800

    Catch errors with gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,15 @@ gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass({
       precision: 3
-    }))
+    }).on('error', errorHandler))
     .pipe(gulp.dest('app/css'))
 });
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+});
+
+function errorHandler(err) {
+    console.log(err.toString());
+    this.emit('end');
+}
