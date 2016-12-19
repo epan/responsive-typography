c190d025e9f4535fea86b209572685ffcb4c6f2b
Eric Pan <ericpanbot@gmail.com>
2016-12-18 23:39:58 -0800

    Use gulp-sass pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-gulp.task('hello', function() {
-  console.log('Hello Eric');
+gulp.task('sass', function() {
+  return gulp.src('app/scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
 });
