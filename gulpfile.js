4a2279b83c15b088463b638a436b3464dcc2e168
Eric Pan <ericpanbot@gmail.com>
2016-12-18 23:52:42 -0800

    Use 3 decimal points precision

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  return gulp.src('app/scss/styles.scss')
-    .pipe(sass())
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass({
+      precision: 3
+    }))
     .pipe(gulp.dest('app/css'))
 });
